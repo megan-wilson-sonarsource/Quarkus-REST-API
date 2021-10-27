b77f2ca7bef239f3cc07d666737134a8c25caab1
Megan Wilson <megan.wilson@sonarsource.com>
2021-10-27 13:08:18 -0500

    initial Quarkus REST API Application that inputs a username returns a Greeting of Hello UserName

---
+++ src/main/java/org/acme/getting/started/GreetingService.java
@@ -0,0 +1,9 @@
+package org.acme.getting.started;
+
+import javax.enterprise.context.ApplicationScoped;
+@ApplicationScoped
+public class GreetingService {
+    public String greeting(String name){
+        return "Hello "+ name;
+    }
+}
